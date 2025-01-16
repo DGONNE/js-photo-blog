a22dcd66656b99c3df655b2bdac02556633d6415
Davide Gonnelli <davide.gonnelli1@gmail.com>
2025-01-16 10:23:46 +0100

    js def

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API di JSON Placeholder, sfruttando la risposta per generare dinamicamente in pagina una serie di foto! */
 
-const photoContainerElm = document.querySelectorAll("card")
+/* const photoContainerElm = document.querySelectorAll("card")
 
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(response => {
 const photos = response.data;
@@ -9,4 +9,13 @@ const image
 photoContainerElm.innerHTML +=
 
 })
-})
+})  */
+
+axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
+      .then(function(response){
+        console.log("response.data", response.data)
+        response.data.forEach((element,index) => {
+            const nameTitle = element.title;
+            const card = document.getElementById("card");
+        })
+      })
